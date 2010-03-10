874817d8ad4ac876ff6af0bd0efad8b373bf33bd
Thomas Kollbach <thomas@kollba.ch>
2010-03-10 11:50:08 +0100

    Initialcommit with nntp2atom.rb

---
+++ README.md
@@ -0,0 +1,27 @@
+mini-scripts
+=====
+
+A small collection of useful scripts and mini-tools I use from day to day
+
+nntp2atom.rb
+----------
+
+Ruby script that turns the last 15 postings NNTP-Newsgroup into an Atom feed. 
+
+Requires:
+
+ - ruby >= 1.8.6
+ - gems
+    - builder
+    - nntp
+
+
+
+License
+=====
+© 2010 Thomas Kollbach <dev@bitfever.de>
+
+Released under the MIT Licence
+
+
+
---
+++ nntp2atom.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/env ruby
+# == Synopsis
+#
+# Transforms a given NNTP newsgroup into an atom feed.
+#
+# == Usage
+#
+# nntp2atom.rb NNTPSERVER GROUPNAME
+require 'rdoc/usage'
+require 'rubygems'
+gem 'nntp'
+require 'nntp'
+gem 'builder'
+require 'builder'
+require 'date'
+
+unless (ARGV & %w{-h --help}).empty?
+  RDoc::usage 
+  exit(-1)
+end
+
+server = ARGV.shift
+group = ARGV.shift
+updated = DateTime.civil(1970)
+
+messages = []
+
+Net::NNTP.start(server, 119) do |nntp|
+  message_ids = nntp.listgroup(group)
+  nntp.group(group)
+  message_ids.last[-10..-1].each do |message_id|
+    message = {:id => message_id}
+    nntp.head(message_id).each do |header|
+      header.each do |line| 
+        if line.index('Subject:') 
+          message[:subject] = line.split(': ').last
+        end
+        if line.index('Date:') 
+          message[:date] = DateTime.parse(line.split(': ').last)
+          updated = message[:date] if message[:date] > updated
+        end
+        if line =~ /^From: /
+          from = line.split(': ').last
+          match = from.match(/(.+) <(.+)@(.+)>/)
+          message[:author] = match[1] rescue (p line)
+          message[:email] = "#{match[2]}@#{match[3]}"
+        end
+      end
+    end    
+    message[:body] = nntp.body(message_id).join("\n")
+    messages << message
+  end
+end
+
+xmlstr = ''
+atom_feed = Builder::XmlMarkup.new(:target => xmlstr)
+atom_feed.feed(:xmlns => 'http://www.w3.org/2005/Atom') do |feed|
+  feed.title("Newsgroup #{server} #{group}")
+  feed.updated(updated.strftime("%Y-%m-%dT%H:%M:%SZ"))
+  uri = "nntp://#{server}/#{group}"
+  feed.link(:href => uri)
+  feed.id(uri)
+
+  for message in messages
+    feed.entry do |entry|
+      uri = "nntp://#{server}/#{group}/#{message[:id]}"
+      entry.link(:href => uri)
+      entry.id(uri)
+      entry.title(message[:subject])
+      entry.summary(message[:body], :type => 'text')      
+      entry.updated(message[:date].strftime("%Y-%m-%dT%H:%M:%SZ")) # needed to work with Google Reader.
+      entry.author do |author|
+        author.name(message[:author])
+        author.email(message[:email])        
+      end
+    end
+  end
+end
+
+puts xmlstr
